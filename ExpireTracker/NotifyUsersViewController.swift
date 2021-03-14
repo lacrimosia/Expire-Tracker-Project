c147ac0af8321970e79650644f199ddafdc24b64
Shatilla Prayer <shatillaprayer@gmail.com>
2021-03-14 11:06:09 -0700

    Enabled switch for toggle in core data

--- ExpireTracker/NotifyUsersViewController.swift
+++ ExpireTracker/NotifyUsersViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class NotifyUsersViewController: UIViewController {
     
@@ -18,8 +19,6 @@ class NotifyUsersViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getCoreDataInfo()
-        
-        // enable notifications
     }
     
     func getCoreDataInfo() {
@@ -32,15 +31,24 @@ class NotifyUsersViewController: UIViewController {
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
             if let coreDataNotifyUpdates = try? context.fetch(Notify.fetchRequest()) as? [Notify] {
                 notif = coreDataNotifyUpdates
+                enableSwitchButton.isOn = notif[0].notifyEnabled
             }
         }
     }
     @IBAction func enableNotifications(_ sender: UISwitch) {
+        // Save the switch settings to core data
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
             let item = Notify(context: context)
             item.notifyEnabled = sender.isOn
+            notif[0].notifyEnabled = item.notifyEnabled
             (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            
         }
+        
+        // enable notifications
+        // fetch to see if core data notify enabled
+        let isNotifEnabled = notif[0].notifyEnabled
+        print(isNotifEnabled)
     }
     /*
      // MARK: - Navigation
